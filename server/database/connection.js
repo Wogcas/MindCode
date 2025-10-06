d9e4d61d5b13b8fb41bb82480a6152e8de28620d
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2025-10-06 07:45:25 -0700

    segunda correccion de la base de datos

--- server/database/connection.js
+++ server/database/connection.js
@@ -11,7 +11,7 @@ import { uri } from "./atlas_uri.js";
  */
 async function connectToDatabase() {
     try {
-        await mongoose.connect('uri');
+        await mongoose.connect(uri);
         console.log(`Connected to the database`);
     } catch (err) {
         console.error(`Error connecting to the database: ${err}`);
