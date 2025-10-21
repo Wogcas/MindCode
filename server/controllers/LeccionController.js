87f7e421725ca946ed2d93b451abdce440f504bd
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-10-20 18:45:23 -0700

    Leccion route y controller

--- server/controllers/LeccionController.js
+++ server/controllers/LeccionController.js
@@ -21,8 +21,87 @@ export default class LeccionController {
             });
 
         }
-
-
     }
+
+    obtenerLecciones = async (req, res) => {
+        try {
+            const lecciones = await leccionService.obtenerLecciones();
+            return res.status(200).json(lecciones);
+        } catch (error) {
+            console.error('Controller Error:', error);
+            res.status(500).json({
+                message: 'Controller Error: Al intentar obtener lecciones',
+                error: error.message
+            });
+        }
+    }
+
+    obtenerLeccionesPorId = async (req, res) => {
+        const id = req.params.id;
+        try {
+            const leccionPorIdObtendida = await leccionService.obtenerLeccionPorId(id);
+            if (!leccionPorIdObtendida) {
+                return res.status(404).json({ message: 'Error: No se encontró la leccion' });
+            }
+            return res.status(200).json(leccionPorIdObtendida);
+        } catch (error) {
+            console.error('Controller Error:', error);
+            res.status(500).json({
+                message: 'Controller Error: Al intentar obtener leccion por id',
+                error: error.message
+            });
+        }
+    }
+
+    obtenerLeccionesPorNombre = async (req, res) => {
+        const nombre = req.params.nombre;
+        try {
+            const leccionesPorNombreObtendida = await leccionService.obtenerLeccionPorNombre(nombre);
+            if (!leccionesPorNombreObtendida || leccionesPorNombreObtendida.length === 0) {
+                return res.status(404).json({ message: 'No se encontraron lecciones con ese nombre' });
+            }
+            return res.status(200).json(leccionesPorNombreObtendida);
+        } catch (error) {
+            console.error('Controller Error:', error);
+            res.status(500).json({
+                message: 'Controller Error: Al intentar obtener lecciones por nombre',
+                error: error.message
+            });
+        }
+    }
+
+    actualizarLeccion = async (req, res) => {
+        const id = req.params.id;
+        const dataLeccion = req.body;
+        try {
+            const leccionActualizada = await leccionService.actualizarLeccion(id, dataLeccion);
+            if (!leccionActualizada) {
+                return res.status(404).json({ message: 'Error: No se encontró la leccion para actualizar' });
+            }
+            return res.status(200).json(leccionActualizada);
+        } catch (error) {
+            console.error('Controller Error:', error);
+            res.status(500).json({
+                message: 'Controller Error: Al intentar actualizar leccion',
+                error: error.message
+            });
+        }
+    }
+
+    eliminarLeccion = async (req, res) => {
+        const id = req.params.id;
+        try {
+            await leccionService.eliminarLeccion(id);
+            return res.status(200).json({ message: "Leccion eliminada exitosamente" });
+        } catch (error) {
+            console.error('Controller Error:', error);
+            res.status(500).json({
+                message: 'Controller Error: Al intentar eliminar leccion',
+                error: error.message
+            });
+        }
+    }
+
+
 
 }
--- server/routes/LeccionRoute.js
+++ server/routes/LeccionRoute.js
@@ -4,6 +4,11 @@ import LeccionController from "../controllers/LeccionController.js"
 const leccionController = new LeccionController();
 const leccionRouter = new Router();
 
-leccionRouter.post('/agregarLeccion', leccionController.agregarLeccion);
+leccionRouter.post('/agregar', leccionController.agregarLeccion);
+leccionRouter.get("/", leccionController.obtenerLecciones);
+leccionRouter.get("/:id", leccionController.obtenerLeccionesPorId);
+leccionRouter.get("/leccion/:nombre", leccionController.obtenerLeccionesPorNombre);
+leccionRouter.put("/actualizar/:id", leccionController.actualizarLeccion);
+leccionRouter.delete("/eliminar/:id", leccionController.eliminarLeccion);
 
 export default leccionRouter;
