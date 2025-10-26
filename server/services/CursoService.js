9444ecff5f837954b3d22014736d9870bed41ea0
Gael Rafael Castro Molina <gael.castro247887@potros.itson.edu.mx>
2025-10-25 22:53:48 -0700

    rest api with good practices
    dtos and adapters on Curso

--- server/services/CursoService.js
+++ server/services/CursoService.js
@@ -1,4 +1,5 @@
 import CursoRepository from "../repositories/CursoRepository.js";
+import { CursoAdapter } from "../utils/adapters/CursoAdapter.js";
 
 export default class CursoService {
 
@@ -8,49 +9,61 @@ export default class CursoService {
 
     async agregarCurso(curso) {
         try {
-            return await this.cursoRepo.agregarCurso(curso);
+            const cursoGuardado = await this.cursoRepo.agregarCurso(curso);
+            return CursoAdapter.toDTO(cursoGuardado);
         } catch (error) {
             console.log('Service Error: While adding new Curso to the database');
+            throw error;
         }
     }
 
     async obtenerCursos() {
         try {
-            return await this.cursoRepo.obtenerCursos();
+            const cursos = await this.cursoRepo.obtenerCursos();
+            return cursos.map(curso => CursoAdapter.toDTO(curso));
         } catch (error) {
             console.log('Service Error: While obtaining all the Cursos from the database');
+            throw error;
         }
     }
 
     async obtenerCursoPorNombre(tituloCurso) {
         try {
-            return await this.cursoRepo.obtenerCursoPorNombre(tituloCurso);
+            const cursos = await this.cursoRepo.obtenerCursoPorNombre(tituloCurso);
+            return cursos.map(curso => CursoAdapter.toDTO(curso));
         } catch (error) {
             console.log('Service Error: While obtaining a Curso by name from the database');
+            throw error;
         }
     }
 
     async obtenerCursoPorId(idCurso) {
         try {
-            return await this.cursoRepo.obtenerCursoPorId(idCurso);
+            const curso = await this.cursoRepo.obtenerCursoPorId(idCurso);
+            return CursoAdapter.toDTO(curso);
         } catch (error) {
             console.log('Service Error: While obtaining a Curso by Id from the database');
+            throw error;
         }
     }
 
     async actualizarCurso(idCurso, cursoModificado) {
         try {
-            return await this.cursoRepo.actualizarCurso(idCurso, cursoModificado)
+            const cursoActualizado = await this.cursoRepo.actualizarCurso(idCurso, cursoModificado);
+            return CursoAdapter.toDTO(cursoActualizado);
         } catch (error) {
             console.log('Service Error: While updating a Curso from the database');
+            throw error;
         }
     }
 
     async eliminarCurso(idCurso) {
         try {
-            return await this.cursoRepo.eliminarCurso(idCurso);
+            const cursoEliminado = await this.cursoRepo.eliminarCurso(idCurso);
+            return CursoAdapter.toDTO(cursoEliminado);
         } catch (error) {
             console.log('Service Error: While deleting a Curso from the database');
+            throw error;
         }
     }
 }
