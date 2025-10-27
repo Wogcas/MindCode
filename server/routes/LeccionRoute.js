c2d1e2bfaaed91666bf7e951619b2d6bf49095c6
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-10-27 00:27:18 -0700

    privatizar rutas

--- server/routes/LeccionRoute.js
+++ server/routes/LeccionRoute.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import LeccionController from "../controllers/LeccionController.js"
+import { esMentor } from "../auth/authMiddleware.js";
 
 const leccionController = new LeccionController();
 const leccionRouter = new Router();
 
-leccionRouter.post('/agregar', leccionController.agregarLeccion);
-leccionRouter.get("/", leccionController.obtenerLecciones);
-leccionRouter.get("/id/:id", leccionController.obtenerLeccionesPorId);
-leccionRouter.get("/titulo/:titulo", leccionController.obtenerLeccionesPorTitulo);
-leccionRouter.put("/:id", leccionController.actualizarLeccion);
-leccionRouter.delete("/:id", leccionController.eliminarLeccion);
+// Rutas protegidas Mentor
+leccionRouter.post('/agregar', verifyToken, esMentor, leccionController.agregarLeccion);
+leccionRouter.get("/", verifyToken, esMentor, leccionController.obtenerLecciones);
+leccionRouter.get("/id/:id", verifyToken, esMentor, leccionController.obtenerLeccionesPorId);
+leccionRouter.get("/titulo/:titulo", verifyToken, esMentor, leccionController.obtenerLeccionesPorTitulo);
+leccionRouter.put("/:id", verifyToken, esMentor, leccionController.actualizarLeccion);
+leccionRouter.delete("/:id", verifyToken, esMentor, leccionController.eliminarLeccion);
 
 export default leccionRouter;
