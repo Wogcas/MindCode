968480069c7cf476b3be3380a2f099b8ae406196
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2025-10-27 00:11:44 -0700

    token para gets
    se agrego la autenticacion a las peticiones get

--- server/routes/RetoRoute.js
+++ server/routes/RetoRoute.js
@@ -5,13 +5,10 @@ import { esMentor, verifyToken } from "../auth/authMiddleware.js";
 const retoRouter = Router();
 const retoController = new RetoController();
 
-// Rutas publicas
-retoRouter.get("/", retoController.obtenerTodosLosRetos); // ya jaló tu
-retoRouter.get("/:id", retoController.obtenerReto); // ya jaló tu
-retoRouter.get("/retoTitulo/:titulo", retoController.obtenerRetoPorTitulo);// ya jaló tu
-retoRouter.get("/retoLeccion/:id", retoController.obtenerRetosPorLeccion); // ya jaló tu
-
-// Rutas protegidas
+retoRouter.get("/", verifyToken, retoController.obtenerTodosLosRetos); // ya jaló tu
+retoRouter.get("/:id", verifyToken, retoController.obtenerReto); // ya jaló tu
+retoRouter.get("/retoTitulo/:titulo", verifyToken, retoController.obtenerRetoPorTitulo);// ya jaló tu
+retoRouter.get("/retoLeccion/:id", verifyToken, retoController.obtenerRetosPorLeccion); // ya jaló tu
 retoRouter.post("/agregar", verifyToken, esMentor, retoController.crearReto);// ya jaló tu
 retoRouter.put("/actualizar/:id", verifyToken, esMentor, retoController.modificarReto); // ya jaló tu
 retoRouter.delete("/eliminar/:id", verifyToken, esMentor, retoController.eliminarReto);// ya jaló tu
