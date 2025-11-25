0a7a0fa10f77e2e604e96c62cab12b0a3ea115ce
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-11-24 23:55:31 -0700

    Correcciones tailwind hechas

--- app/src/dashboard/componentes/bienvenida.js
+++ app/src/dashboard/componentes/bienvenida.js
@@ -1,15 +1,19 @@
 class Bienvenida extends HTMLElement {
-    connectedCallback() {
-        const text = this.getAttribute("text") || "Titulo";
-        const profile = this.getAttribute("profile") || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+  connectedCallback() {
+    const text = this.getAttribute("text") || "Título";
+    const profile = this.getAttribute("profile")
+      || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
 
-        this.innerHTML = `
-      <div>
-        <p>${profile}</p>
-        <img src="${text}" class="w-full h-40 object-cover rounded-lg" />
+    this.innerHTML = `
+      <div class="flex items-center gap-4 p-4">
+        <img 
+          src="${profile}" 
+          class="w-20 h-20 object-cover rounded-full border"
+        />
+        <p class="text-lg font-semibold">${text}</p>
       </div>
     `;
-    }
+  }
 }
 
-customElements.define("bienvenida", Bienvenida);
+customElements.define("bienvenida-usuario", Bienvenida);
